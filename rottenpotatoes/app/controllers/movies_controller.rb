a6e425918a54a10240a793e616604d910040e486
EC2 Default User <ec2-user@ip-172-31-15-234.us-east-2.compute.internal>
2021-02-28 11:47:51 +0000

    Finish Everything besides rspec

--- rottenpotatoes/app/controllers/movies_controller.rb
+++ rottenpotatoes/app/controllers/movies_controller.rb
@@ -37,11 +37,22 @@ class MoviesController < ApplicationController
     flash[:notice] = "Movie '#{@movie.title}' deleted."
     redirect_to movies_path
   end
+  
+  def same_director
+    @movie = Movie.find(params[:id])
+    
+    if @movie.director == "" || @movie.director == nil
+      redirect_to movies_path
+      flash[:notice] = "'#{@movie.title}' has no director info"
+    else
+      @movies = Movie.same_director(@movie.director)
+    end
+  end
 
   private
   # Making "internal" methods private is not required, but is a common practice.
   # This helps make clear which methods respond to requests, and which ones do not.
   def movie_params
-    params.require(:movie).permit(:title, :rating, :description, :release_date)
+    params.require(:movie).permit(:title, :rating, :description, :release_date, :director)
   end
 end
